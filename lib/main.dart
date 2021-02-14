e615e76786dbed0d32a960dbbd4ea62fc1e33051
Charith Bhat <charsept04@gmail.com>
2021-02-14 11:54:48 +0530

    small test

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends ConsumerWidget {
 
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Example test')),
+        appBar: AppBar(title: Text('Example')),
         body: Center(
           child: Text(
             value,
