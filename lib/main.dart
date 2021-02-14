86ec0324b3ada225f76ca62b1e3d962e9865e9f6
Charith Bhat <charsept04@gmail.com>
2021-02-14 11:20:26 +0530

    checking out riverPod

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final helloWorldProvider = Provider((_) => 'Hello World');
+
+void main() {
+  runApp(
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    final String value = watch(helloWorldProvider);
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Example')),
+        body: Center(
+          child: Text(
+            value,
+            style: TextStyle(fontSize: 30),
+          ),
+        ),
+      ),
+    );
+  }
+}
